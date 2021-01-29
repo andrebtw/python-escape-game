9ff69fdb85a6efa617e57d84bb5b28e636c33c14
andrebtw <43089870+andrebtw@users.noreply.github.com>
2021-01-29 10:56:46 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,9 @@
-codes=[0:"d",1:"w"]
+def temp_translator(x):
+  temp_codes=["d","t","r","8","u","p","x","z","e","p"]
+  return temp_codes.index(x)
+
+
+while True:
+    a=input("type : ")
+    print(temp_translator(a))
+    
