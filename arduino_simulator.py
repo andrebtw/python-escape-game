595040bc81aef998247050a2e8f0ca14db86d0bf
andrebtw <gwandrewgd@gmail.com>
2021-03-14 11:12:37 +0100

    update

--- arduino_simulator.py
+++ arduino_simulator.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter
 
 gui = Tk(className='ARDUINO SIMULATION')
 gui.geometry("800x500")
@@ -6,10 +7,10 @@ gui.configure(bg='black')
 
 
 
-taking_temps = tkinter.Button(root, text="TEMP", bg="blue", width=20, height=5)
+taking_temps = tkinter.Button(gui, text="TEMP", bg="blue", width=20, height=5)
 taking_temps.place(x=20, y=200)
 
-info = tkinter.Button(root, text="INFOS", bg="red", width=20, height=5)
+info = tkinter.Button(gui, text="INFOS", bg="red", width=20, height=5)
 info.place(x=630, y=200)
 
 
