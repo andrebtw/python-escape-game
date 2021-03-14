dd2cc1a9096b26f34eaa8b96e54e0bf0cacab052
andrebtw <gwandrewgd@gmail.com>
2021-03-14 11:10:13 +0100

    update

--- arduino_simulator.py
+++ arduino_simulator.py
@@ -1,12 +1,19 @@
-import Tkinter
-import tkMessageBox
+from tkinter import *
 
-top = Tkinter.Tk()
+gui = Tk(className='ARDUINO SIMULATION')
+gui.geometry("800x500")
+gui.configure(bg='black')
 
-def helloCallBack():
-   tkMessageBox.showinfo( "Hello Python", "Hello World")
 
-B = Tkinter.Button(top, text ="Hello", command = helloCallBack)
 
-B.pack()
-top.mainloop()
+taking_temps = tkinter.Button(root, text="TEMP", bg="blue", width=20, height=5)
+taking_temps.place(x=20, y=200)
+
+info = tkinter.Button(root, text="INFOS", bg="red", width=20, height=5)
+info.place(x=630, y=200)
+
+
+
+
+
+gui.mainloop()
