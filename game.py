c0fdcc092e9126e5ac6deb0bf326b1d8cc0f4841
andrebtw <gwandrewgd@gmail.com>
2021-04-06 20:28:25 +0200

    update

--- game.py
+++ game.py
@@ -11,33 +11,33 @@ tens = ('Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Nin
 suffixes = ('', 'Thousand', 'Million', 'Billion')
 
 def process(number, index):
-    
+
     if number=='0':
         return 'Zero'
-    
+
     length = len(number)
-    
+
     if(length > 3):
         return False
-    
+
     number = number.zfill(3)
     words = ''
- 
+
     hdigit = int(number[0])
     tdigit = int(number[1])
     odigit = int(number[2])
-    
+
     words += '' if number[0] == '0' else ones[hdigit]
     words += ' Hundred ' if not words == '' else ''
-    
+
     if(tdigit > 1):
         words += tens[tdigit - 2]
         words += ' '
         words += ones[odigit]
-    
+
     elif(tdigit == 1):
         words += twos[(int(tdigit + odigit) % 10) - 1]
-        
+
     elif(tdigit == 0):
         words += ones[odigit]
 
@@ -45,22 +45,22 @@ def process(number, index):
         words = words[:-len('Zero')]
     else:
         words += ' '
-     
-    if(not len(words) == 0):    
+
+    if(not len(words) == 0):
         words += suffixes[index]
-        
+
     return words;
-    
+
 def getWords(number):
     length = len(str(number))
-    
+
     if length>12:
         return 'This program supports upto 12 digit numbers.'
-    
+
     count = length // 3 if length % 3 == 0 else length // 3 + 1
     copy = count
     words = []
- 
+
     for i in range(length - 1, -1, -3):
         words.append(process(str(number)[0 if i - 2 < 0 else i - 2 : i + 1], copy - count))
         count -= 1;
@@ -69,11 +69,12 @@ def getWords(number):
     for s in reversed(words):
         temp = s + ' '
         final_words += temp
-    
+
     return final_words
 # End Main Logic
 
-###SOURCE CODE FOR CONVERTING NUMBERS INTO LETTERS : https://www.codesansar.com/python-programming-examples/number-words-conversion-no-library-used.htm###
+#SOURCE CODE FOR CONVERTING NUMBERS INTO LETTERS : https://www.codesansar.com/python-programming-examples/number-words-conversion-no-library-used.htm
+
 
 
 
@@ -84,7 +85,7 @@ temp =  open("temp.txt", "r")
 temp=temp.read()
 
 temp=str(temp)
-temp = temp[:-3] 
+temp = temp[:-3]
 
 
 
@@ -106,7 +107,3 @@ final_string= getWords(first_character+second_character)
 
 
 print(final_string)
-
-
-
-
