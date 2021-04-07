6cbab9abbe65139f74799657ba871a227b132cbe
andrebtw <gwandrewgd@gmail.com>
2021-04-07 11:22:43 +0200

    update

---
+++ Arduino/display.ino
@@ -0,0 +1,27 @@
+#include <LiquidCrystal.h>
+int Contrast=0;
+ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+
+void lcd_print(text, x, y, clear) {
+  if (clear == true) {
+    lcd.clear();
+  }
+
+  lcd.setCursor(x,y);
+
+  lcd.print(text);
+
+}
+
+
+ void setup()
+ {
+   analogWrite(6,Contrast);
+   lcd.begin(16, 2);
+  }
+
+void loop()
+{
+  lcd_print("THIS IS A TEST TEXT", 4,0,true);
+}
