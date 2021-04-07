d41dc30c07da682a3035dd04253f3480c28c433f
andrebtw <gwandrewgd@gmail.com>
2021-04-07 11:35:00 +0200

    update

--- Arduino/display.ino
+++ Arduino/display.ino
@@ -1,30 +1,35 @@
+//INITIALISATION
 #include <LiquidCrystal.h>
 int Contrast=0;
- LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 
 
+
+//CLEAR LINE FUNCTION
 void clear_line(int y) {
   lcd.setCusor(0,y);
   lcd.print("                "); //clear all the line
 }
 
 
+
+//PRINT LCD
 void lcd_print(char text[] ,int x, int  y, bool clear_line, bool clear_all) {
   if (clear_line == true) {
     clear_line(y);
   }
-
   if (clear_all == true) {
     lcd.clear();
   }
 
   lcd.setCursor(x,y);
-
   lcd.print(text);
-
 }
 
+
+
+//PRINT LCD MOVING CHARACTERS
 void lcd_print_move(char text[] ,int x, int  y, bool clear, int steps, int milliseconds) {
   if (clear == true) {
     lcd.clear();
@@ -33,11 +38,11 @@ void lcd_print_move(char text[] ,int x, int  y, bool clear, int steps, int milli
   }
 
 
- void setup()
- {
+void setup()
+{
    analogWrite(6,Contrast);
    lcd.begin(16, 2);
-  }
+ }
 
 void loop()
 {
