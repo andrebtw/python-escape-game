92335d2e75b652abfe28d3e9d76726056935be0a
andrebtw <gwandrewgd@gmail.com>
2021-04-07 11:33:58 +0200

    update clear line

--- Arduino/display.ino
+++ Arduino/display.ino
@@ -3,8 +3,19 @@ int Contrast=0;
  LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 
-void lcd_print(char text[] ,int x, int  y, bool clear) {
-  if (clear == true) {
+
+void clear_line(int y) {
+  lcd.setCusor(0,y);
+  lcd.print("                "); //clear all the line
+}
+
+
+void lcd_print(char text[] ,int x, int  y, bool clear_line, bool clear_all) {
+  if (clear_line == true) {
+    clear_line(y);
+  }
+
+  if (clear_all == true) {
     lcd.clear();
   }
 
@@ -30,6 +41,6 @@ void lcd_print_move(char text[] ,int x, int  y, bool clear, int steps, int milli
 
 void loop()
 {
-  lcd_print("THIS IS A TEST TEXT", 1,0,false);
+  lcd_print("THIS IS A TEST TEXT", 1,0,false, false);
   lcd_print("TEST", 6,1,false);
 }
