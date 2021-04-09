495f1a5c5ecf0e550d3edccd5d0a2cf5736480e7
andrebtw <gwandrewgd@gmail.com>
2021-04-09 23:46:22 +0200

    update

---
+++ Arduino/main/info.ino
@@ -0,0 +1,29 @@
+int info_count=1;
+
+
+void info() {
+    if (info_count == 1) {
+      info_count=2;
+      }
+
+    else if (info_count == 2) {
+      info_count=3;
+      }
+    else if (info_count == 3) {
+      info_count=1;
+      }
+  }
+
+
+
+
+
+void setup()
+{
+  
+}
+
+void loop()
+{
+
+}
