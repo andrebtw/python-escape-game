38537bb5a4e674c982a38489cb3626d5e6b47a74
andrebtw <gwandrewgd@gmail.com>
2021-04-17 17:12:36 +0200

    update

--- Arduino/main/temp_potentiometer.ino
+++ Arduino/main/temp_potentiometer.ino
@@ -3,47 +3,47 @@ void temp_potentiometer() {
   int sensorValue = analogRead(A0);
 
   if (sensorValue >=0 && sensorValue <=100) {
-    temp=20.05;
+    temp=18;
   }
 
   else if (sensorValue >=100 && sensorValue <=200) {
-    temp=20.55;
+    temp=19;
   }
 
   else if (sensorValue >=200 && sensorValue <=300) {
-    temp=21.05;
+    temp=20;
   }
 
   else if (sensorValue >=300 && sensorValue <=400) {
-    temp=21.55;
+    temp=21;
   }
 
   else if (sensorValue >=400 && sensorValue <=500) {
-    temp=22.05;
+    temp=22;
   }
 
   else if (sensorValue >=500 && sensorValue <=600) {
-    temp=22.55;
+    temp=23;
   }
 
   else if (sensorValue >=600 && sensorValue <=700) {
-    temp=23.05;
+    temp=24;
   }
 
   else if (sensorValue >=700 && sensorValue <=800) {
-    temp=23.55;
+    temp=25;
   }
 
   else if (sensorValue >=800 && sensorValue <=900) {
-    temp=24.05;
+    temp=26;
   }
 
   else if (sensorValue >=900 && sensorValue <=1000) {
-    temp=24.55;
+    temp=27;
   }
 
   else if (sensorValue >=1000 && sensorValue <=1100) {
-    temp=25.05;
+    temp=28;
   }
 
 
