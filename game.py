23019f50661b151b0a0a1c97053b992237146053
andrebtw <gwandrewgd@gmail.com>
2021-04-29 14:49:03 +0200

    update

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 import serial
 import re
+import sys
 
 # MIC CFG
 recognizer = sr.Recognizer()
@@ -9,83 +10,7 @@ recognizer = sr.Recognizer()
 import serial
 ser = serial.Serial('COM4')
 
-
-#####
-# Number to Words
-
-# Main Logic
-ones = ('Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine')
-
-twos = ('Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen')
-
-tens = ('Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety', 'Hundred')
-
-suffixes = ('', 'Thousand', 'Million', 'Billion')
-
-def process(number, index):
-
-    if number=='0':
-        return 'Zero'
-
-    length = len(number)
-
-    if(length > 3):
-        return False
-
-    number = number.zfill(3)
-    words = ''
-
-    hdigit = int(number[0])
-    tdigit = int(number[1])
-    odigit = int(number[2])
-
-    words += '' if number[0] == '0' else ones[hdigit]
-    words += ' Hundred ' if not words == '' else ''
-
-    if(tdigit > 1):
-        words += tens[tdigit - 2]
-        words += ' '
-        words += ones[odigit]
-
-    elif(tdigit == 1):
-        words += twos[(int(tdigit + odigit) % 10) - 1]
-
-    elif(tdigit == 0):
-        words += ones[odigit]
-
-    if(words.endswith('Zero')):
-        words = words[:-len('Zero')]
-    else:
-        words += ' '
-
-    if(not len(words) == 0):
-        words += suffixes[index]
-
-    return words;
-
-def getWords(number):
-    length = len(str(number))
-
-    if length>12:
-        return 'This program supports upto 12 digit numbers.'
-
-    count = length // 3 if length % 3 == 0 else length // 3 + 1
-    copy = count
-    words = []
-
-    for i in range(length - 1, -1, -3):
-        words.append(process(str(number)[0 if i - 2 < 0 else i - 2 : i + 1], copy - count))
-        count -= 1;
-
-    final_words = ''
-    for s in reversed(words):
-        temp = s + ' '
-        final_words += temp
-
-    return final_words
-# End Main Logic
-
-#SOURCE CODE FOR CONVERTING NUMBERS INTO LETTERS : https://www.codesansar.com/python-programming-examples/number-words-conversion-no-library-used.htm
+ser = str(sys.argv[1])
 
 
 
@@ -142,33 +67,3 @@ while True :
             print("CORRECT")
         else:
             print("nope xd")
-
-
-
-temp =open("temp.txt", "r")
-
-temp=temp.read()
-
-temp=str(temp)
-temp = temp[:-3]
-
-
-
-
-first_character=temp[0]
-second_character=temp[1]
-
-first_character=int(first_character)
-second_character=int(second_character)
-
-first_character=first_character**2
-second_character=second_character**2
-
-final_number=first_character+second_character
-
-print(final_number)
-
-final_string= getWords(first_character+second_character)
-
-
-print(final_string)
