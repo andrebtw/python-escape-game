b120a35f7ee8db07907e760017da5cec0a13d203
andrebtw <gwandrewgd@gmail.com>
2021-04-29 16:02:53 +0200

    update test

---
+++ test.py
@@ -0,0 +1,19 @@
+#importation des librairies importantes
+
+import speech_recognition as sr
+import serial
+import re
+import sys
+
+# MIC CFG
+recognizer = sr.Recognizer()
+
+# ARDUINO CFG
+import serial
+
+ser = str(sys.argv[1])
+
+
+
+while True :
+    arduino.write(bytes("Adjusting Noise",'utf-8'))
