82a7e0eb948ec87f8a76a87c3ee1bb70cddd085b
andrebtw <gwandrewgd@gmail.com>
2021-04-29 10:35:05 +0200

    update

--- Arduino/main/rec.ino
+++ Arduino/main/rec.ino
@@ -1,3 +1,14 @@
 void rec () {
   Serial.println("recording");
+  String incoming;
+
+  if(Serial.available() > 0) {
+    incoming = Serial.read();
+  }
+  if(incoming == 1) {
+    yourFunction();
+  } else {
+    errorFunction();
+  }
+
   }
